d5ea067e8ff03bdf00b077a68c672cabdc780732
Thomas Keller <thomaskeller@microsoft.com>
2024-11-08 21:52:33 +0000

    Initial InferRotaryEmbeddingImplementation

--- src/finn/transformation/fpgadataflow/convert_to_hw_layers.py
+++ src/finn/transformation/fpgadataflow/convert_to_hw_layers.py
@@ -1697,3 +1697,77 @@ class InferVectorVectorActivation(Transformation):
             model = model.transform(InferShapes())
             model = model.transform(InferDataTypes())
         return (model, graph_modified)
+
+
+
+class InferRoPE(Transformation):
+    """Convert any Rotary Position Embedding (RoPE) node 
+    to the HW implementation layer.
+    Supported RoPE node: https://github.com/microsoft/onnxruntime/blob/main/docs/ContribOperators.md#com.microsoft.RotaryEmbedding
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def apply(self, model):
+        graph = model.graph
+        node_ind = 0
+        graph_modified = False
+        for node in graph.node:
+            node_ind += 1
+            if node.op_type == "RotaryEmbedding" and node.domain == "com.microsoft":
+                # Get inputs/output
+                act_in = node.input[0]
+                pos_ids = node.input[1]
+                cos = node.input[2]
+                sin = node.input[3]
+                act_out = node.output[0]
+                # Get attributes
+                interleaved = getCustomOp(node).get_nodeattr("interleaved")
+                packed_batch = getCustomOp(node).get_nodeattr("is_packed_batching")
+                num_heads = getCustomOp(node).get_nodeattr("num_heads")
+                rot_dim = getCustomOp(node).get_nodeattr("rotary_embedding_dim")
+                scale = getCustomOp(node).get_nodeattr("scale")
+                # Get custom attributes (not in Op by default)
+                rope_theta = getCustomOp(node).get_nodeattr("RopeTheta")
+                # Get any needed tensor info
+                shape_in = model.get_tensor_shape(act_in)
+                idt = model.get_tensor_datatype(act_in)
+                odt = model.get_tensor_datatype(act_out)
+
+                # We assume input order [batch, seq_len, hidden_dim]
+                # TODO: Add logic to handle NCW cases
+                seq_len=shape_in[-2]
+                hidden_dim=shape_in[-1]
+                
+                # TODO: Handle 
+                num_input_vectors = ???
+
+                # TODO: Apply any constraints for HW implementation
+
+                # Create node with no parallelization first
+                simd = 1
+                assert hidden_dim % simd == 0, "Requirement channel divisable by SIMD is violated."
+                # Create and insert node
+                new_node = helper.make_node(
+                    "RotaryEmbedding",
+                    [act_in, cos, sin],
+                    [act_out],
+                    domain="finn.custom_op.fpgadataflow",
+                    backend="fpgadataflow",
+                    SequenceLength=seq_len,
+                    HiddenDimension=hidden_dim,
+                    HeadDimension=hidden_dim//num_heads,
+                    NumHeads=num_heads,
+                    simd=simd,
+                    inputDataType=idt.name,
+                    outputDataType=odt.name,
+                    numInputVectors=num_input_vectors,
+                    name="RotaryEmbedding_" + node.name,
+                )
+                graph.node.insert(node_ind, new_node)
+                # remove old node
+                graph.node.remove(node)
+                graph_modified = True
+
+        return (model, graph_modified)
